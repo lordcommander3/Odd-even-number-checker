0cca5a2040558145b2da32026d0efee2c7d476f5
zaryab <zaryabsheikh6@gmail.com>
2022-03-09 19:24:14 +0500

    odd or even checker

---
+++ using else if/app.js
@@ -0,0 +1,9 @@
+    var a=prompt("Enter your number here");
+    var b=2;
+    if(a % b == 0){
+            document.write(`${a} is an even number`)
+        }
+        else{
+            document.write(`${a} is an odd number`)
+        }
+    
---
+++ using ternery operator/app.js
@@ -0,0 +1,4 @@
+var a = prompt("Enter your value here");
+var b = 2;
+var c = a % b == 0 ? "Even" : "Odd";
+var x= document.getElementById("box_1").innerText=`${a} is an ${c} number`;
